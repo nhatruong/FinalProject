f25b330705de1afc8dc25a86d39acd8ece02ec1b
ruchengxiao <31635807+ruchengxiao@users.noreply.github.com>
2017-12-02 11:56:58 -0800

    Modified

--- NeuSep17-Project-Group2/src/com/neuSep17/dto/Dealer.java
+++ NeuSep17-Project-Group2/src/com/neuSep17/dto/Dealer.java
@@ -4,23 +4,27 @@ public class Dealer {
     private String id;
     private String name;
     private String url;
-    
-    public Dealer() {
-    	
-    }
-    public Dealer(String id, String name, String url) {
-    	this.id=id; this.name=name; this.url=url;
+    private String emailId;
+    private String contactNumber;
+
+    public Dealer() {}
+
+    public Dealer(String id, String name, String url, String emailId, String contactNumber) {
+        this.id=id; this.name=name; this.url=url; this.emailId = emailId; this.contactNumber = contactNumber;
     }
     public String getId() {
         return id;
     }
-    
     public void setId(String id) {
         this.id = id;
     }
     public String getName() {
         return name;
     }
+
+    public String getEmailId() { return emailId;}
+    public String getContactNumber() { return contactNumber; }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -30,4 +34,11 @@ public class Dealer {
     public void setUrl(String url) {
         this.url = url;
     }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
+
